0872376382e1c14dae1689ed1f81eb38fb431cdb
Paul Krapf <PaulKrapf@gmail.com>
2021-03-21 16:18:30 -0500

    first commit

--- 01-java-fundamentals/08-lists-of-exceptions/src/ListOfExceptions.java
+++ 01-java-fundamentals/08-lists-of-exceptions/src/ListOfExceptions.java
@@ -2,23 +2,21 @@ import java.util.ArrayList;
 
 class ListOfExceptions {
 
-    public static void main(String[] args) {
-        ArrayList<Object> myList = new ArrayList<Object>();
-        myList.add("13");
-        myList.add("hello world");
-        myList.add(48);
-        myList.add("Goodbye World");
+	public static void main(String[] args) {
+		ArrayList<Object> myList = new ArrayList<Object>();
+		myList.add("13");
+		myList.add("hello world");
+		myList.add(48);
+		myList.add("Goodbye World");
 
-
-        for(int i = 0; i < myList.size(); i++) {
-            try {
-                int castedValue = (Integer) myList.get(i);
-                System.out.println(castedValue);
-            } catch(ClassCastException e ) {
-                System.out.println(e);
-            }
-        }
-    }
-
+		for (int i = 0; i < myList.size(); i++) {
+			try {
+				int castedValue = (int) myList.get(i);
+				System.out.println(castedValue);
+			} catch (ClassCastException e) {
+				System.out.println(e);
+			}
+		}
+	}
 
 }

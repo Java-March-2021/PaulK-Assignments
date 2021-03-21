f781e8c708972f9312fe150850384d646b1f6297
Paul Krapf <PaulKrapf@gmail.com>
2021-03-21 16:35:10 -0500

    first commit

---
+++ 02-java-oop/01-project-class/src/ProjectTest.java
@@ -0,0 +1,11 @@
+public class ProjectTest {
+
+    public static void main(String[] args) {
+        Project advertisingProject = new Project();
+        advertisingProject.setName("Acme Advertising");
+        advertisingProject.setCost(300000);
+        advertisingProject.setDesc("Acme Corporation. AAdvertosomg campaign for Acme Corporation's new product line of widgets for 2021");
+        System.out.println(advertisingProject.elevatorPitch());
+    }
+    
+}

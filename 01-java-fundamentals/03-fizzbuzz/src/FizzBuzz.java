9b57dd8d57e49fd5aa7ff783c91b75c39b9d0743
Paul Krapf <PaulKrapf@gmail.com>
2021-03-21 15:28:55 -0500

    first commit

---
+++ 01-java-fundamentals/03-fizzbuzz/src/FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); // Create a Scanner object
+		System.out.println("Enter number");
+
+		int number = input.nextInt(); // Read user input
+		if (number % 15 == 0) {
+			System.out.println("FizzBuzz");
+		} else if (number % 3 == 0) {
+			System.out.println("Fizz");
+		} else if (number % 5 == 0) {
+			System.out.println("Buzz");
+		} else {
+			System.out.println(number);
+		}
+	}
+
+//	public static void FizzBuzzTest() {
+//		for (int i = 0; i < 100; i++) {
+//			String result = fizzBuzz(i);
+//			String output = String.format("Number: %d :: Result: %s", i, result);
+//			System.out.println(output);
+//		}
+//	}
+}

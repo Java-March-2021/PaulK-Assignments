7a1ce6972ce5d3e85b715ecb4740e6b536bcb62a
Paul Krapf <PaulKrapf@gmail.com>
2021-04-15 14:03:42 -0500

    firsst commit

--- 04-java-spring/01-spring-fundamentals/06-the-code/src/main/java/com/paul/thecode/controller/HomeController.java
+++ 04-java-spring/01-spring-fundamentals/06-the-code/src/main/java/com/paul/thecode/controller/HomeController.java
@@ -1,34 +1,28 @@
 package com.paul.thecode.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Controller
 public class HomeController {
 
 	@RequestMapping("/")
-	public String code() {
+	public String index() {
 		return "index.jsp";
 	}
 
-	@RequestMapping("/bushido")
-	public String bushido() {
-		return "bushido.jsp";
-	}
+	@RequestMapping(path = "/code", method = RequestMethod.POST)
+	public String code(@RequestParam(value = "code") String code, RedirectAttributes redirect) {
+//		if (code.equals("bushido")) {
+		if (code == "bushido") {
+			return "bushido.jsp";
 
-	@RequestMapping(path = "/attempt", method = RequestMethod.POST)
-	public String Attempt(@RequestParam(value = "guess") String guess) {
-		if (guess == "bushido") {
-			return "redirect:/bushido";
 		} else {
-			return "redirect:/error";
+			redirect.addFlashAttribute("error", "You must train harder!");
+			return "redirect:/";
 		}
-	}
-
-	@RequestMapping("/error")
-	public String error(RedirectAttributes redirectAttributes) {
-		redirectAttributes.addFlashAttribute("error", "You must try harder!");
-		return "redirect:/error";
 	}
 }
